fa4c33424ade0807432288f85ba80852a2fc6fe1
hgomezmeneses <74203177+hgomezmeneses@users.noreply.github.com>
2020-11-10 13:40:38 +0100

    Update pictures_downloader.py

--- source/pictures_downloader.py
+++ source/pictures_downloader.py
@@ -10,13 +10,17 @@ from bs4 import BeautifulSoup
 import re
 import pandas as pd
 import urllib.request
-
+#Creamos el directorio
 os.mkdir('Album_Pictures')
 j=0
+#Para cada página web, cada cual contiene diez álbumes
 for i in range(51):
+    # Cargamos el contenido
     page = requests.get("https://www.besteveralbums.com/overall.php?o=&f=&fv=&orderby=-InfoRankScore&sortdir=asc&page=" + str(1))
     soup = BeautifulSoup(page.content, 'html.parser')
+    # Filtramos todas las imágenes
     pictures = soup.find_all('picture')
+    # Descargamos las imágenes y las guardamos en el directorio creado
     for picture in pictures:
         url = 'https://www.besteveralbums.com/' + picture.source.get('data-srcset')
         path = 'Album_Pictures/album' + str(j) +'.jpg' 
